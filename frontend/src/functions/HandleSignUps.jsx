65d80db5b890bcb7df107627ee64e2d8065b871e
Toad <111982929+BestToad57@users.noreply.github.com>
2024-12-07 23:38:27 -0500

    Im tired
    Pushing this, and going to Sleep
    
    Render has to be re-work since now there is a database interworking

--- frontend/src/functions/HandleSignUps.jsx
+++ frontend/src/functions/HandleSignUps.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const HandleSignUp = async ({username, }) => {
+const HandleSignUp = async ({username, password}) => {
     try {
         const response = await axios.post('http://localhost:5000/signup', {username, password});
         alert(response.data.message);
